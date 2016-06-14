6b6529f9fc457eebbb87e1e07ff9f525cc42707f
Kirill <timofeev.kirill.a@gmail.com>
2016-06-14 15:34:53 +0300

    errors exceptions refactored

--- src/main/kotlin/im/kirillt/jbtask/CompilerError.kt
+++ src/main/kotlin/im/kirillt/jbtask/CompilerError.kt
@@ -8,23 +8,25 @@ open class CompilerError(val msg: String) {
     }
 }
 
-open class ErrorInStatement(msg: String, method: Method, statement: Statement)
-: CompilerError("$msg in statement '$statement' in method '$method'")
+class ErrorsInMethod(val method: Method, val errors: List<CompilerError>)
 
-open class UnknownVariable(variable: Variable, method: Method, statement: Statement)
-: ErrorInStatement("Unknown variable '${variable.name}'", method, statement)
+open class ErrorInStatement(msg: String, statement: Statement)
+: CompilerError("$msg in statement '$statement'")
 
-open class UnknownType(type: Type, method: Method, statement: Statement)
-: ErrorInStatement("Unknown type '${type.name}'", method, statement)
+open class UnknownVariable(variable: Variable, statement: Statement)
+: ErrorInStatement("Unknown variable '${variable.name}'", statement)
 
-open class WrongReturnType(type: Type, method: Method, statement: Statement)
-: ErrorInStatement("Wrong return type '${type.name}'", method, statement)
+open class UnknownType(type: Type, statement: Statement)
+: ErrorInStatement("Unknown type '${type.name}'", statement)
+
+open class WrongReturnType(type: Type, statement: Statement)
+: ErrorInStatement("Wrong return type '${type.name}'", statement)
+
+open class ReAssignToFinal(variable: Variable, statement: Statement)
+: ErrorInStatement("${variable.name} cannot be re-assigned", statement)
 
 open class NoSuchMethod(where: ClassOrInterface, method: Method)
 : CompilerError("${where.name} have no method $method")
 
 open class NoSuchField(where: ClassOrInterface, field: Field)
 : CompilerError("${where.name} have no field ${field.name}")
-
-open class ReAssignToFinal(variable: Variable, method: Method, statement: Statement)
-: ErrorInStatement("${variable.name} cannot be re-assigned", method, statement)
